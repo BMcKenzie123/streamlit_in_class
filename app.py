334c3901da898c79d5b59a8cb557ac1dd79efa03
BMcKenzie123 <bmckenzie3@my.gcu.edu>
2026-01-14 15:40:37 -0700

    Feat: Initial test push

---
+++ app.py
@@ -0,0 +1,9 @@
+
+import streamlit as st
+
+st.title("Hello, Streamlit! 👋")
+st.write("This is my first Streamlit app!")
+
+name = st.text_input("What's your name?")
+if name:
+    st.write(f"Hello, {name}!")
